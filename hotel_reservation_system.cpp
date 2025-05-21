c3b47545f3a158b099057b4061ae00f157b25347
Kevin-byte721 <djk6174@gmail.com>
2025-05-21 09:11:35 +0800

    third commit

--- hotel_reservation_system.cpp
+++ hotel_reservation_system.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-#include <iomanip>
 #include <string>
 
 using namespace std;
 
-// Function to print currency in pesos and centavos
+// Strategy for room pricing
+class RoomPriceStrategy {
+public:
+    virtual double getPrice() const = 0;
+    virtual ~RoomPriceStrategy() {}
+};
+
+class RoomAPriceStrategy : public RoomPriceStrategy {
+public:
+    double getPrice() const override { return 2000.0; }
+};
+
+class RoomBPriceStrategy : public RoomPriceStrategy {
+public:
+    double getPrice() const override { return 1500.0; }
+};
+
+class RoomCPriceStrategy : public RoomPriceStrategy {
+public:
+    double getPrice() const override { return 1000.0; }
+};
+
 void printCurrency(double value) {
     if (value < 0) {
         cout << "-";
@@ -27,24 +47,30 @@ bool isRoomAvailable(char room) {
     return false;
 }
 
-// Strategy for room pricing
-class RoomPriceStrategy {
+class HotelReservationSystem {
+private:
+    RoomPriceStrategy* priceStrategy;
+    char selectedRoom;
 public:
-    virtual double getPrice() const = 0;
-    virtual ~RoomPriceStrategy() {}
-};
-
-class RoomAPriceStrategy : public RoomPriceStrategy {
-public:
-    double getPrice() const override { return 2000.0; }
-};
-
-class RoomBPriceStrategy : public RoomPriceStrategy {
-public:
-    double getPrice() const override { return 1500.0; }
-};
-
-class RoomCPriceStrategy : public RoomPriceStrategy {
-public:
-    double getPrice() const override { return 1000.0; }
+    HotelReservationSystem() : priceStrategy(nullptr), selectedRoom(' ') {}
+    void setRoomPriceStrategy(RoomPriceStrategy* strategy, char room) {
+        delete priceStrategy;
+        priceStrategy = strategy;
+        selectedRoom = room;
+    }
+    void displayBill() const {
+        if (!priceStrategy) {
+            cout << "Error: No room selected.\n";
+            return;
+        }
+        double price = priceStrategy->getPrice();
+        cout << "\n--- Receipt ---\n";
+        cout << "Room " << selectedRoom << " Price: ";
+        printCurrency(price);
+        cout << " Pesos\n";
+        cout << "Thank you for your payment!\n";
+    }
+    ~HotelReservationSystem() { delete priceStrategy; }
+    HotelReservationSystem(const HotelReservationSystem&) = delete;
+    HotelReservationSystem& operator=(const HotelReservationSystem&) = delete;
 };
